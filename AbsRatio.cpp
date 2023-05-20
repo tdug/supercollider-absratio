f89ff93ce6c0eff85a82311b3d056e7047d27d66
Tom Dugovic <tom.dugovic@gmail.com>
2023-05-20 14:21:18 -0500

    init

---
+++ AbsRatio.cpp
@@ -0,0 +1,27 @@
+#include "SC_PlugIn.hpp"
+
+static InterfaceTable *ft;
+
+struct AbsRatio : public SCUnit {
+    public:
+        AbsRatio() {
+            set_calc_function<AbsRatio, &AbsRatio::next>();
+            next(1);
+        }
+
+    private:
+        void next(int inNumSamples) {
+            const float* x = in(0);
+            const float* y = in(1);
+            float* outbuf = out(0);
+
+            for (int i = 0; i < inNumSamples; i++) {
+                outbuf[i] = (x > y) ? (y/x) : (x/y);
+            }
+        }
+}
+
+PluginLoad(AbsRatio) {
+    ft = inTable;
+    registerUnit<AbsRatio>(ft, "AbsRatio")
+}
---
+++ AbsRatio.sc
@@ -0,0 +1,12 @@
+AbsRatio : UGen {
+    *kr {|x,y| ^this.multiNew('control', x, y) }
+    *ar {|x,y| ^this.multiNew('audio', x, y) }
+}
+
+SimpleNumber : {
+    .absRatio { |other| ^(this > other).if({other / this}, {this / other}) }
+}
+
+UGen {
+    .absRatio { |other| ^AbsRatio(this, other) }
+}
